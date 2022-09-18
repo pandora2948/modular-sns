6f92f72a7d42ff7eba3dad3f9f9dd0ba6acf080e
최은기 <galaxyhi4276@gmail.com>
2022-09-18 23:33:40 +0900

    feat: header 초안 작성

--- react-happy-hacking/src/components/Header/Header.jsx
+++ react-happy-hacking/src/components/Header/Header.jsx
@@ -1,21 +1,33 @@
-import { Button } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import { Button, Avatar, Input } from 'antd';
+import { SearchOutlined, UserOutlined } from '@ant-design/icons';
 
 export const Header = () => {
+  const [openSearch, setOpenSearch] = useState(false);
+
   return (
     <header
       className="
-        flex h-14 bg-white justify-between px-4 items-center
+        flex h-14 bg-white justify-between px-4 items-center shadow-md
+        relative
       "
     >
-      <h1>sns-modular</h1>
-      <nav>
+      <h1 className="text-lg text-slate-600">sns-modular</h1>
+      <nav className="flex gap-x-2">
+        <Avatar icon={<UserOutlined />} />
         <Button
-          type="primary"
+          color="#0ea5e9"
+          type="default"
           shape="circle"
           icon={<SearchOutlined />}
+          onClick={() => setOpenSearch(prev => !prev)}
         />
       </nav>
+      {openSearch &&
+        <div className="absolute right-1 -bottom-10 transition ease-in">
+          <Input.Search />
+        </div>
+      }
     </header>
   );
 };
