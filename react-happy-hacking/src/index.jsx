255c774e06660fe59f89a876961fdb26287c4e6c
최은기 <galaxyhi4276@gmail.com>
2022-09-19 23:24:34 +0900

    feat: react-query 초기 설정 완료

--- react-happy-hacking/src/index.jsx
+++ react-happy-hacking/src/index.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClientProvider, QueryClient } from 'react-query';
 
 import App from './App';
 import './config/styles.css';
 import { worker } from './mocks/worker';
 
-if (process.env.NODE_ENV === 'development') {
-  worker.start().then(r => {
-    console.log('MSW 워커 서비스가 시작되었습니다.');
-    console.log(r);
-  });
-}
+const isDevelopment = process.env.NODE_ENV === 'development';
+if (isDevelopment) worker.start();
+
+const queryClient = new QueryClient();
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
