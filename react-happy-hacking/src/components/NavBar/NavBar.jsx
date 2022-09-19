adcf59266e26bc40f8c4e3f92c7886079d74671f
Baek DongJae <pandora2948@naver.com>
2022-09-19 21:30:58 +0900

    style: navBar height modified

--- react-happy-hacking/src/components/NavBar/NavBar.jsx
+++ react-happy-hacking/src/components/NavBar/NavBar.jsx
@@ -1,13 +1,12 @@
 import { PlusCircleFilled } from '@ant-design/icons';
-import { useState } from 'react';
 
 export const NavBar = ({
   buttons = [...new Array(5)].map((el, i) => (
-    <PlusCircleFilled style={{ fontSize: '1.8em' }} key={'navBtn_' + i} />
+    <PlusCircleFilled style={{ fontSize: '1.8em' }} />
   )),
 }) => {
   return (
-    <nav className="relative h-10 inset-x-0 flex bottom-0 bg-white px-4 py-2 justify-between items-center">
+    <nav className="relative h-12 inset-x-0 flex bottom-0 bg-white px-4 py-2 justify-between items-center">
       {buttons.map((btn) => (
         <button>{btn}</button>
       ))}
