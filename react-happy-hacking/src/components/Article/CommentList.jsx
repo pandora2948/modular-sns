d533af1ebfc51aad81daafdfb3fe5fbb8d122963
최은기 <galaxyhi4276@gmail.com>
2022-09-20 11:52:58 +0900

    feat: 댓글 더보기 구현

--- react-happy-hacking/src/components/Article/CommentList.jsx
+++ react-happy-hacking/src/components/Article/CommentList.jsx
@@ -2,6 +2,7 @@ import { Typography } from 'antd';
 import PropTypes from 'prop-types';
 import { UserOutlined } from '@ant-design/icons';
 import shortid from 'shortid';
+import { useState } from 'react';
 
 const { Text } = Typography;
 
@@ -15,15 +16,38 @@ export const Comment = ({ comment }) => (
   </div>
 );
 
-export const CommentList = ({ comments }) => (
-  <section className="flex flex-col justify-center p-3">
-    {
-      comments?.map(comment => (
-        <Comment comment={comment} />
-      ))
-    }
-  </section>
-);
+export const CommentList = ({ comments }) => {
+  const [expand, setExpand] = useState(false);
+
+  const onClickHandleExpand = () => setExpand(prev => !prev);
+  const notExpand = !expand && comments.length >= 1;
+
+  return (
+    <section className="flex flex-col justify-center p-3">
+      {notExpand && (
+          <Comment comment={comments[0]} />
+      )
+      }
+      {expand &&
+        comments?.map(comment => (
+            <Comment comment={comment} />
+        ))
+      }
+      {notExpand && <Text
+        className="text-sky-500 cursor-pointer"
+        onClick={onClickHandleExpand}
+      >
+        댓글 더보기
+      </Text>}
+      {expand && <Text
+        className="text-sky-500 cursor-pointer"
+        onClick={onClickHandleExpand}
+      >
+        댓글 닫기
+      </Text>}
+    </section>
+  );
+};
 
 CommentList.propTypes = {
   comments: PropTypes.arrayOf(
