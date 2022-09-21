4e50f92c1d980b2b0e71be6a0acf5e202c51747d
최은기 <galaxyhi4276@gmail.com>
2022-09-21 22:38:26 +0900

    feat: 검색 창 해시태그 파싱 로직 작성

--- react-happy-hacking/src/components/Header/Search.jsx
+++ react-happy-hacking/src/components/Header/Search.jsx
@@ -1,9 +1,18 @@
 import PropTypes from 'prop-types';
-import { Input } from 'antd';
+import { Input, message } from 'antd';
 import { useEffect, useRef } from 'react';
 
 export const Search = ({ open, setOpen }) => {
   const inputRef = useRef(null);
+
+  const onSearch = searchText => {
+    if (searchText.trim() === '')
+      return message.error('해시태그를 검색해주세요.');
+    const hashtags = searchText.match(/(#+[가-힣a-zA-Z0-9]+)/g);
+    console.log(hashtags);
+    if (hashtags.length > 5)
+      return message.error('해시태그 5개 이상 검색할 수 없습니다.');
+  };
 
   useEffect(() => {
     const onBlur = () => setOpen(false);
@@ -25,7 +34,10 @@ export const Search = ({ open, setOpen }) => {
   if (!open) return null;
   return (
     <div className="absolute right-1 -bottom-12 z-10">
-      <Input.Search size="large" ref={inputRef} />
+      <Input.Search
+        size="large" ref={inputRef}
+        onSearch={onSearch}
+      />
     </div>
   );
 };
