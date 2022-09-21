3545198b79e9e814d7871dbd0314842806f8f645
최은기 <galaxyhi4276@gmail.com>
2022-09-21 22:47:53 +0900

    feat: 404 에러 페이지 작성 및 애플리케이션 적용

--- react-happy-hacking/src/index.jsx
+++ react-happy-hacking/src/index.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import {  createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
 
 import { Feed } from './pages/Feed';
 import './config/styles.css';
 import { worker } from './mocks/worker';
+import { Error } from './pages/Error';
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 if (isDevelopment) worker.start();
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <Feed />,
+    errorElement: <Error />
+  }
+])
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
+    <RouterProvider router={router}>
       <React.StrictMode>
         <Feed />
       </React.StrictMode>
-    </BrowserRouter>
+    </RouterProvider>
   </QueryClientProvider>
 );
---
+++ react-happy-hacking/src/pages/Error.jsx
@@ -0,0 +1,24 @@
+import { Typography } from 'antd';
+import { WarningFilled } from '@ant-design/icons';
+
+import { AppLayout } from '../layouts/AppLayout';
+import { Link } from 'react-router-dom';
+
+const { Text } = Typography;
+
+export const Error = () => (
+  <AppLayout>
+    <section className="w-full h-full flex flex-col items-center justify-center">
+      <WarningFilled style={{ fontSize: '5rem' }} />
+      <Text className="text-2xl mt-5">
+        해당하는 페이지가 없습니다.
+      </Text>
+      <Link
+        to="/"
+        className="text-sky-600 text-xl"
+      >
+        메인으로 돌아가기
+      </Link>
+    </section>
+  </AppLayout>
+);
