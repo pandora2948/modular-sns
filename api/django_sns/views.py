36d0c2f005c4482dcbaabfe4999c4108231bd932
최은기 <galaxyhi4276@gmail.com>
2022-09-25 13:42:43 +0900

    feat: 장고 백엔드 애플리케이션 단 분리, restframework 초안 구조 작성

---
+++ api/django_sns/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+
+class UserAPI(APIView):
+  def get(self, req):
+    print(req)
+    queryset = User.objects.all()
+    print(queryset)
+    serializer = UserSerializer(queryset, many=True)
+    return Response(serializer.data)
--- api/src/settings.py
+++ api/src/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_sns',
     'src',
     'rest_framework',
 ]
@@ -119,3 +120,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST FRAMEWORK 설정
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer'
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
