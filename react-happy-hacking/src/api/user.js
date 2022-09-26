d5126d3e071cd4cfb5488546a454df6134d6a6f9
최은기 <galaxyhi4276@gmail.com>
2022-09-27 07:19:18 +0900

    feat: 사용자 로그인, 회원가입 api 작성

--- react-happy-hacking/src/api/user.js
+++ react-happy-hacking/src/api/user.js
@@ -9,3 +9,37 @@ export const getUserApi = () =>
   apiClient.get('/api/user')
     .then(parseSuccessResponse)
     .catch(parseFailureResponse);
+
+
+/**
+ * @description 인증을 요청하여 토큰 값을 반환받습니다.
+ * @param email 로그인 이메일 정보
+ * @param password 로그인 패스워드 정보
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const loginUserApi = (email, password) =>
+  apiClient.post('/api/user/login', {
+    email, password,
+  })
+    .then(parseSuccessResponse)
+    .catch(parseFailureResponse);
+
+/**
+ * @description 사용자 회원가입 요청입니다. 사용 전 폼 검증 필수입니다.
+ * @param username 회원가입 받을 사용자 이름
+ * @param email 회원가입 받을 사용자 이메일
+ * @param password 회원가입 받을 사용자 패스워드
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const createUserApi = ({
+  username,
+  email,
+  password,
+}) =>
+  apiClient.post('/api/user/register', {
+    username,
+    email,
+    password
+  })
+    .then(parseSuccessResponse)
+    .then(parseFailureResponse);
