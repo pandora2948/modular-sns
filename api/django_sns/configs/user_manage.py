c6298788ed39b1d7951ce5a3cc37b2a707138fad
최은기 <galaxyhi4276@gmail.com>
2022-09-27 21:02:17 +0900

    feat: 사용자 관리 클래스 작성

---
+++ api/django_sns/configs/user_manage.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+  def create_user(self, username: str, email: str, password: str):
+    if not username:
+      raise ValueError('must be exists username')
+    if not email:
+      raise ValueError('must be exists email')
+    if not password:
+      raise ValueError('must be exists password')
+
+    user = self.model(
+      username=username,
+      email=self.normalize_email(email),
+    )
+    user.set_password(password)
+    user.save()
+    return user
+
+  def create_admin(self):
+    pass
