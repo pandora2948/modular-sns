a07e87a4d8769e41d58f3d27d3620d6da5ba9f6e
최은기 <galaxyhi4276@gmail.com>
2022-11-01 12:26:53 +0900

    feat: 회원가입 및 로그인 요청 dto 작성 및 jpa auditing 클래스 작성

---
+++ restful-server/src/main/java/com/modular/restfulserver/auth/dto/UserLoginRequestDto.java
@@ -0,0 +1,32 @@
+package com.modular.restfulserver.auth.dto;
+
+import com.modular.restfulserver.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginRequestDto {
+  private String email;
+  private String password;
+  private String username;
+
+  public User toEntity() {
+    return User.builder()
+      .addEmail(email)
+      .addUsername(username)
+      .addPassword(password)
+      .build();
+  }
+
+  public void setEncodePassword(String encodePassword) {
+    this.password = encodePassword;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+}
---
+++ restful-server/src/main/java/global/utils/models/BaseTimeAuditing/CreateAndModifiedTimeAuditEntity.java
@@ -0,0 +1,25 @@
+package global.utils.models.BaseTimeAuditing;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class CreateAndModifiedTimeAuditEntity {
+
+  @CreatedDate
+  private LocalDateTime createdDate;
+
+  @LastModifiedDate
+  private LocalDateTime updatedDate;
+
+  @Temporal(TemporalType.TIMESTAMP)
+  private Date deletedDate;
+}
