75cb0ac68fb449317fa9045081d66ae1e755971e
최은기 <galaxyhi4276@gmail.com>
2022-11-05 19:40:34 +0900

    feat: 토큰 예외 처리 전역 예외 처리 핸들러에 등록

--- restful-server/src/main/java/com/modular/restfulserver/global/exception/GlobalExceptionHandler.java
+++ restful-server/src/main/java/com/modular/restfulserver/global/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.modular.restfulserver.global.exception;
 
 import com.modular.restfulserver.auth.exception.AlreadyExistsUserException;
+import com.modular.restfulserver.auth.exception.InvalidTokenException;
+import com.modular.restfulserver.auth.exception.NoneTokenOnHeaderException;
 import com.modular.restfulserver.auth.exception.PasswordNotMatchException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,6 +28,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
   @ExceptionHandler(UsernameNotFoundException.class)
   protected ResponseEntity<ErrorResponse> handleUserNotFoundException() {
     return ErrorResponse.toResponseEntity(USER_NOT_FOUND);
+  }
+
+  @ExceptionHandler(NoneTokenOnHeaderException.class)
+  protected ResponseEntity<ErrorResponse> handleNoneTokenOnHeaderException() {
+    return ErrorResponse.toResponseEntity(NONE_TOKEN_ON_HEADER);
+  }
+
+  @ExceptionHandler(InvalidTokenException.class)
+  protected ResponseEntity<ErrorResponse> handleInvalidTokenException() {
+    return ErrorResponse.toResponseEntity(INVALID_TOKEN);
   }
 
 }
