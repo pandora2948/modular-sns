66061091af73eaa5dbb40e0a0c2792ccd1a1a27b
최은기 <galaxyhi4276@gmail.com>
2022-11-06 20:15:39 +0900

    test: 테스트용 단순 문자열 반환 인가 필요 api 작성

---
+++ restful-server/src/main/java/com/modular/restfulserver/auth/api/AuthTestApi.java
@@ -0,0 +1,16 @@
+package com.modular.restfulserver.auth.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class AuthTestApi {
+
+  @GetMapping("/")
+  public String authorizationTest() {
+    return "fulfilled";
+  }
+
+}
