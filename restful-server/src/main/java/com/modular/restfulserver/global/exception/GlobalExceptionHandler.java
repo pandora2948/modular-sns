0228678a3bf3588d820e34b821cd5c5ec4f50437
최은기 <galaxyhi4276@gmail.com>
2022-11-07 11:57:47 +0900

    feat: 리스코프 치환 법칙 준수

--- restful-server/src/main/java/com/modular/restfulserver/global/exception/GlobalExceptionHandler.java
+++ restful-server/src/main/java/com/modular/restfulserver/global/exception/GlobalExceptionHandler.java
@@ -1,43 +1,19 @@
 package com.modular.restfulserver.global.exception;
 
-import com.modular.restfulserver.auth.exception.AlreadyExistsUserException;
-import com.modular.restfulserver.auth.exception.InvalidTokenException;
-import com.modular.restfulserver.auth.exception.NoneTokenOnHeaderException;
-import com.modular.restfulserver.auth.exception.PasswordNotMatchException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import static com.modular.restfulserver.global.exception.ErrorCode.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler(AlreadyExistsUserException.class)
-  protected ResponseEntity<ErrorResponse> handleAlreadyUserException() {
-    return ErrorResponse.toResponseEntity(ALREADY_EXISTS_USER);
-  }
-
-  @ExceptionHandler(PasswordNotMatchException.class)
-  protected ResponseEntity<ErrorResponse> handlePasswordNotMatchedException() {
-    return ErrorResponse.toResponseEntity(PASSWORD_NOT_MATCH);
-  }
-
-  @ExceptionHandler(UsernameNotFoundException.class)
-  protected ResponseEntity<ErrorResponse> handleUserNotFoundException() {
-    return ErrorResponse.toResponseEntity(USER_NOT_FOUND);
-  }
-
-  @ExceptionHandler(NoneTokenOnHeaderException.class)
-  protected ResponseEntity<ErrorResponse> handleNoneTokenOnHeaderException() {
-    return ErrorResponse.toResponseEntity(NONE_TOKEN_ON_HEADER);
-  }
-
-  @ExceptionHandler(InvalidTokenException.class)
-  protected ResponseEntity<ErrorResponse> handleInvalidTokenException() {
-    return ErrorResponse.toResponseEntity(INVALID_TOKEN);
+  @ExceptionHandler(CustomException.class)
+  protected ResponseEntity<ErrorResponse> handleAlreadyUserException(
+    CustomException exception
+  ) {
+    return ErrorResponse.toResponseEntity(exception.getErrorCode());
   }
 
 }
