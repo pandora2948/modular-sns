5a8f0cc9299f619d2dca1ab5e5cedb3cd53a9cc2
Will Kim <will@chequer.io>
2022-11-07 23:01:48 +0900

    Chore

--- react-happy-hacking/src/components/nav/Nav.jsx
+++ react-happy-hacking/src/components/nav/Nav.jsx
@@ -4,20 +4,28 @@ import shortid from 'shortid';
 import Marketplace from './Marketplace';
 
 const Nav = () => {
-  const { isModalOpen, toggleModal, openModal } = useModal();
+  const {
+    isModalOpen: isMarketPlaceModalOpen,
+    toggleModal: toggleMarketPlaceModal,
+    openModal: openMarketPlaceModal,
+  } = useModal();
 
   return (
     <nav>
-      <Marketplace isOpen={isModalOpen} onClick={toggleModal} />
       <section className="fixed h-12 inset-x-0 bottom-0 bg-white px-4 py-2 flex justify-between">
         {Array(5)
           .fill(0)
           .map(() => (
-            <button key={shortid.generate()} onClick={openModal}>
+            <button key={shortid.generate()} onClick={openMarketPlaceModal}>
               <PlusCircleFilled style={{ fontSize: '1.8em' }} />
             </button>
           ))}
       </section>
+
+      <Marketplace
+        isOpen={isMarketPlaceModalOpen}
+        onClick={toggleMarketPlaceModal}
+      />
     </nav>
   );
 };
