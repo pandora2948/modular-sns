3fb9729cbaff0211d13b046e722c1ae696d354f8
Will Kim <will@chequer.io>
2022-11-19 20:16:37 +0900

    Fix: client.js 토큰 재발급 로직 에러 fix, req res err 로깅 추가

--- src/api/client.js
+++ src/api/client.js
@@ -1,56 +1,58 @@
 import { AuthService } from 'api/services';
-import axios from 'axios';
-import { token } from 'utils';
+import axios, { AxiosError } from 'axios';
+import { token, printRequestLog, printResponseLog, printErrorLog } from 'utils';
 
 const API_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : '<production_url>';
-const authRoutes = ['/auth/login', '/auth/signup'];
-const authErrorStatusList = [401, 403];
+const authRoutes = ['/auth/login', '/auth/signup', '/token/reissue'];
+const tokenErrorStatusList = [401, 403];
 
 const _axios = axios.create({
   baseURL: API_URL,
   timeout: 5000,
 });
 
-_axios.interceptors.request.use(
-  async (config) => {
-    const { accessToken, refreshToken } = token.get();
+_axios.interceptors.request.use(async (config) => {
+  printRequestLog({
+    method: config.method,
+    endPoint: config.url,
+    requestObj: config.data,
+    config,
+  });
 
-    // 둘 다 없으면 에러 날거 알지만 받는 곳에서 에러 컨트롤 할거니까 일단 패스
-    // TODO: abort | cancel 하면서 리디렉션 처리 가능?
-    if (!accessToken && !refreshToken) {
-      return config;
-    }
-
-    // accessToken 있다면 헤더에 세팅 후 요청
-    if (accessToken) {
-      config.headers['authorization'] = `Bearer ${accessToken}`;
-      return config;
-    }
-
-    try {
-      // accessToken 없다면 리프레쉬 토큰으로 새 토큰 받아서 세팅 후 요청
-      const {
-        data: { accessToken: newAccessToken },
-      } = await AuthService.reIssueAccessToken({ refreshToken });
-      token.accessToken.set(newAccessToken);
-      config.headers['authorization'] = `Bearer ${newAccessToken}`;
-      return config;
-    } catch {
-      // 에러 날거 알지만 받는 곳에서 에러 컨트롤 할거니까 일단 패스
-      return config;
-    }
-  },
-  undefined,
-  {
-    synchronous: true,
-    runWhen: (config) => {
-      // auth 요청이면 토큰 당연히 없으므로 패스
-      return !authRoutes.includes(config.url);
-    },
+  if (authRoutes.includes(config.url)) {
+    // console.warn('auth 요청이면 토큰 당연히 없으므로 패스');
+    return config;
   }
-);
 
-const axiosWrapper = async (method, route, body, config) => {
+  const { accessToken, refreshToken } = token.get();
+
+  // TODO: abort | cancel 하면서 리디렉션 처리 가능?
+  if (!accessToken && !refreshToken) {
+    // console.warn('둘 다 없으면 에러 날거 알지만 받는 곳에서 에러 컨트롤 할거니까 일단 패스');
+    return config;
+  }
+
+  if (accessToken) {
+    // console.warn('accessToken 있다면 헤더에 세팅 후 요청');
+    config.headers['authorization'] = `Bearer ${accessToken}`;
+    return config;
+  }
+
+  try {
+    // console.warn('accessToken 없다면 리프레쉬 토큰으로 새 토큰 받아서 세팅 후 요청');
+    const {
+      data: { accessToken: newAccessToken },
+    } = await AuthService.reIssueAccessToken({ refreshToken });
+    token.accessToken.set(newAccessToken);
+    config.headers['authorization'] = `Bearer ${newAccessToken}`;
+    return config;
+  } catch {
+    // console.warn('에러 날거 알지만 받는 곳에서 에러 컨트롤 할거니까 일단 패스');
+    return config;
+  }
+}, undefined);
+
+const axiosWrapper = async (method, route, body, config = {}) => {
   let bodyArr = [];
   switch (method) {
     case 'get':
@@ -76,18 +78,40 @@ const axiosWrapper = async (method, route, body, config) => {
       data: { data },
     } = await _axios[method](...bodyArr);
 
-    if (data.error) {
-      throw data;
-    }
+    printResponseLog({
+      method,
+      endPoint: route,
+      responseObj: data,
+    });
 
     return data;
   } catch (e) {
-    if (e.isAxiosError && authErrorStatusList.includes(e.status)) {
-      window.location = '/';
+    const {
+      response: { data, status },
+    } = e;
+
+    const errorMessage =
+      data.data.message /* server error */ ??
+      data.data.error /* server error */ ??
+      data.error.message /* http error */ ??
+      data.message /* http error */ ??
+      data.error /* http error */ ??
+      e.message; /* http error */
+
+    printErrorLog({
+      method,
+      endPoint: route,
+      errorMessage,
+      errorObj: e,
+    });
+
+    if (e instanceof AxiosError && tokenErrorStatusList.includes(status)) {
+      console.warn('토큰 에러 발생: 401, 403');
+      window.location = '/auth/sign-in';
       return;
     }
-    console.log(e);
-    throw new Error(e.error?.message ?? e.error ?? e);
+
+    throw new Error(errorMessage);
   }
 };
 
