8f0d599334fe8a5299a3a60ab6fca610c90ff97c
Will Kim <will@chequer.io>
2022-11-19 18:43:43 +0900

    feat: Nav 제거

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,13 +1,11 @@
 import PropTypes from 'prop-types';
 import Header from 'components/header/Header';
-import Nav from 'components/nav/Nav';
 
 const AppLayout = ({ children }) => (
-  <main className="flex flex-col justify-between w-full h-screen md:w-[500px] bg-[#F0F2F5] relative">
+  <div className="flex flex-col justify-between w-full h-screen md:w-[500px] bg-[#F0F2F5] relative">
     <Header />
-    <section className="h-full overflow-y-scroll">{children}</section>
-    <Nav />
-  </main>
+    <main className="h-full overflow-y-scroll">{children}</main>
+  </div>
 );
 
 AppLayout.propTypes = {
