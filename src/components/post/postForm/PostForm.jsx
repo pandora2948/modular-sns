f674de0362e568199de02578ad85f5f107a1e511
Will Kim <will@chequer.io>
2022-11-20 17:34:56 +0900

    feat: 게시글 작성 폼 UX 향상 작업

--- src/components/post/postForm/PostForm.jsx
+++ src/components/post/postForm/PostForm.jsx
@@ -1,8 +1,10 @@
 import { useCallback, useState } from 'react';
 import { ArrowLeftOutlined, FormOutlined } from '@ant-design/icons';
-import { Button, Form, Input, Modal } from 'antd';
+import { Button, Form, Input, Modal, message } from 'antd';
 import { PostsService } from 'api/services';
 import { useModal } from 'hooks/useModal';
+
+const TEXT_CONTENT_MAX_LENGTH = 140;
 
 const PostForm = () => {
   const [form] = Form.useForm();
@@ -10,17 +12,28 @@ const PostForm = () => {
   const { isModalOpen, openModal, closeModal } = useModal();
 
   const onChangeText = useCallback((e) => {
-    const { value } = e.target;
-    if (value.length > 140) return; // antd TextArea maxLength is not work
-
-    setText(value);
+    setText(e.target.value);
   }, []);
 
-  const handleSubmit = useCallback(async ({ textContent }) => {
-    try {
-      await PostsService.createPost({ textContent });
-    } catch (e) {}
-  }, []);
+  const handleCloseModal = useCallback(() => {
+    form.resetFields();
+    setText('');
+    closeModal();
+  }, [closeModal, form]);
+
+  const handleSubmit = useCallback(
+    async ({ textContent }) => {
+      try {
+        await PostsService.createPost({
+          textContent,
+        });
+        handleCloseModal();
+      } catch (e) {
+        await message.error(e);
+      }
+    },
+    [handleCloseModal]
+  );
 
   return (
     <>
@@ -35,7 +48,7 @@ const PostForm = () => {
       <Modal
         title={
           <header className="flex justify-between">
-            <Button type="text" shape="circle" icon={<ArrowLeftOutlined />} onClick={closeModal} />
+            <Button type="text" shape="circle" icon={<ArrowLeftOutlined />} onClick={handleCloseModal} />
             <Button type="primary" onClick={form.submit} disabled={!text}>
               작성하기
             </Button>
@@ -43,7 +56,7 @@ const PostForm = () => {
         }
         closable={false}
         open={isModalOpen}
-        onCancel={closeModal}
+        onCancel={handleCloseModal}
         footer={null}
         className="w-full h-full absolute top-0 left-0 m-0 max-w-full bg-white shadow-none full-content header-padding-3"
         maskStyle={{
@@ -52,6 +65,7 @@ const PostForm = () => {
         bodyStyle={{
           padding: 0,
         }}
+        destroyOnClose
       >
         <Form form={form} onFinish={handleSubmit} className="w-full h-full flex flex-col items-end p-3">
           <Form.Item name="textContent" noStyle>
@@ -59,7 +73,9 @@ const PostForm = () => {
               value={text}
               onChange={onChangeText}
               placeholder="무슨 일이 일어나고 있나요?"
-              className="h-36 placeholder-gray-500 text-base"
+              className="w-full h-36 placeholder-gray-500 text-base"
+              maxLength={TEXT_CONTENT_MAX_LENGTH}
+              showCount={({ count, maxLength }) => `${count} / ${maxLength}`}
             />
           </Form.Item>
         </Form>
