87a370b1c575f9f652f14d3793c1daa959cc1d4c
Baek Dongjae <pandora2948@naver.com>
2022-11-26 14:47:31 +0900

    feat: 프로필 페이지 내 게시물 불러오기 기능 작성

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useState } from 'react';
+import { message } from 'antd';
+import shortid from 'shortid';
+import { PostsService } from '../../api/services';
 import EmptyFeed from '../../components/empty/EmptyFeed';
+import PostCard from '../../components/post/postCard/PostCard';
 import UserPanel from '../../components/userPanel/UserPanel';
 import AppLayout from '../../layouts/AppLayout';
 
 const Profile = () => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    PostsService.getUserPosts(0, 3)
+      .then((data) => setPosts(data))
+      .catch((e) => message.error(e));
+  }, []);
   return (
     <AppLayout>
       <UserPanel />
-      <EmptyFeed />
+      {posts.length === 0 ? <EmptyFeed /> : posts.map((post) => <PostCard key={shortid.generate()} post={post} />)}
     </AppLayout>
   );
 };
