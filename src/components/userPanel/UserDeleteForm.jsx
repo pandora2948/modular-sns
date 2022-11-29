91b7f874c2d3a7f6b0421f310a376cb2004e310d
Baek DongJae <pandora2948@naver.com>
2022-11-29 14:37:09 +0900

    fix: 계정 삭제 form 관련 문제 해결

--- src/components/userPanel/UserDeleteForm.jsx
+++ src/components/userPanel/UserDeleteForm.jsx
@@ -1,3 +1,5 @@
+import { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Button, Form, Input, message } from 'antd';
 import { useRecoilValue } from 'recoil';
 import { UserService } from '../../api/services';
@@ -14,10 +16,16 @@ const UserDeleteForm = () => {
   const loginInfo = useRecoilValue(loginInfoState);
   const { formValidateTrigger, onFormFinishFailed, hasFeedback } = useFormValidateTrigger();
   const [form] = Form.useForm();
+  const navigate = useNavigate();
 
-  const deleteUser = ({ password }) => {
-    UserService.deleteLoginedUser({ password }).catch((err) => message.error(err));
-  };
+  const deleteUser = useCallback(
+    async ({ password }) => {
+      await UserService.deleteLoginedUser({ password })
+        .then(() => navigate('/'))
+        .catch((err) => message.error(err));
+    },
+    [navigate]
+  );
 
   return (
     <div className="w-full flex flex-col">
@@ -26,37 +34,38 @@ const UserDeleteForm = () => {
         onFinish={deleteUser}
         onFinishFailed={onFormFinishFailed}
         form={form}
-        scrollToFirstError={formValidateTrigger}
+        scrollToFirstErrorvalidateTrigger={formValidateTrigger}
         {...layout}
       >
         <Form.Item
           label={`${loginInfo.userName} 을 입력해주세요`}
           name="username"
           rules={[
-            requiredRule,
-            (getFieldValue) => ({
+            () => ({
               validator(_, value) {
                 if (value !== loginInfo.userName) {
                   return Promise.reject(new Error('사용자 명이 일치하지 않습니다.'));
                 }
+                return Promise.resolve();
               },
             }),
           ]}
         >
           <Input />
         </Form.Item>
-        <Form.Item label="비밀번호" name="password">
-          <Input.Password
-            allowClear
-            rules={[
-              {
-                pattern: passwordRegex,
-                message: '올바른 비밀번호를 입력해주세요',
-              },
-              requiredRule,
-            ]}
-            hasFeedback={hasFeedback}
-          />
+        <Form.Item
+          label="비밀번호"
+          name="password"
+          rules={[
+            {
+              pattern: passwordRegex,
+              message: '올바른 비밀번호를 입력해주세요',
+            },
+            requiredRule,
+          ]}
+          hasFeedback={hasFeedback}
+        >
+          <Input.Password allowClear />
         </Form.Item>
         <Form.Item>
           <Button type="danger" htmlType="submit">
