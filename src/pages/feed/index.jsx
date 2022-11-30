7c3d341f57527f52450a8996f72679a362eeb6ba
최은기 <galaxyhi4276@gmail.com>
2022-11-30 22:32:10 +0900

    refactor: 사용하지 않거나 불필요한 코드 정리

--- src/pages/feed/index.jsx
+++ src/pages/feed/index.jsx
@@ -13,36 +13,13 @@ export const postsState = atom({ key: 'postsState', default: [] });
 const Feed = () => {
   const [postCount] = useState(3);
   const [posts, setPosts] = useRecoilState(postsState);
-  useEffect(() => {
-    PostsService.getPosts(0, 3)
-      .then((posts) => {
-        setPosts(posts);
-      })
-      .catch((e) => message.error(e));
-
-    return () => {};
-  }, [postCount, setPosts]);
-
   const containerRef = useRef(null);
 
   useEffect(() => {
-    let observer;
-    if (containerRef.current) {
-      observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach(({ _isIntersecting, _boundingClientRect, _intersectionRect }) => {
-            // log.info(isIntersecting, boundingClientRect, intersectionRect);
-          });
-        },
-        {
-          threshold: 0.6,
-        }
-      );
-      observer.observe(containerRef.current);
-    }
-
-    return () => observer?.disconnect();
-  }, []);
+    PostsService.getPosts({ page: 0, size: 999 })
+      .then((posts) => setPosts(posts))
+      .catch((e) => message.error(e));
+  }, [postCount, setPosts]);
 
   return (
     <AppLayout>
