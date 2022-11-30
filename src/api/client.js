00011cb61a2fa476e4ea2513f03f18009e6a6a60
최은기 <galaxyhi4276@gmail.com>
2022-12-01 01:04:28 +0900

    fix: 새로고침 이슈 임시 제거

--- src/api/client.js
+++ src/api/client.js
@@ -61,7 +61,8 @@ const axiosWrapper = async (method, route, body, config = {}) => {
       return data;
     } catch (e) {
       token.clear();
-      window.location = '/auth/sign-in';
+      console.log('기존 리다이렉트');
+      // window.location = '/auth/sign-in';
     }
   }
 };
