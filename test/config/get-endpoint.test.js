6f9b10498f0ab1e24cbff065cc4a9a60e20b1749
최은기 <galaxyhi4276@gmail.com>
2022-12-01 22:20:07 +0900

    refactor: api-endpoint 테스트 줄여봄

--- test/config/get-endpoint.test.js
+++ test/config/get-endpoint.test.js
@@ -1,17 +1,9 @@
 import { getServerUrlByMode } from '../../src/api/helper';
 
 describe('환경에 따른 API ENDPOINT 변경 테스트', () => {
-  let REACT_APP_MODE;
+  it('개발 환경이 development 일 때, localhost:8080 url domain 을 얻는다.', () =>
+    expect(getServerUrlByMode('development')).toBe('http://localhost:8080'));
 
-  it('개발 환경이 development 일 때, localhost:8080 url domain 을 얻는다.', () => {
-    REACT_APP_MODE = 'development';
-    const domain = getServerUrlByMode(REACT_APP_MODE);
-    expect(domain).toBe('http://localhost:8080');
-  });
-
-  it('개발 환경이 test 일 때, http://3.35.123.154:8080 url domain 을 얻는다.', () => {
-    REACT_APP_MODE = 'test';
-    const domain = getServerUrlByMode(REACT_APP_MODE);
-    expect(domain).toBe('http://3.35.123.154:8080');
-  });
+  it('개발 환경이 test 일 때, http://3.35.123.154:8080 url domain 을 얻는다.', () =>
+    expect(getServerUrlByMode('test')).toBe('http://3.35.123.154:8080'));
 });
