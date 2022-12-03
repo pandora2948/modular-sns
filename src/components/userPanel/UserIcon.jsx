60e509e5d820e85170a0bde531c0f0e08b5ec4ed
Will Kim <will@chequer.io>
2022-12-03 18:37:12 +0900

    refactor: UserIcon 리팩터링

--- src/components/userPanel/UserIcon.jsx
+++ src/components/userPanel/UserIcon.jsx
@@ -1,33 +1,27 @@
-import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { UserOutlined } from '@ant-design/icons';
 
-const UserIcon = ({ userName, size = 's' }) => {
-  const [iconSize, setIconSize] = useState(
-    'absolute left-0 bottom-0 bg-blue-500 rounded-full flex justify-center items-center'
-  );
-  useEffect(() => {
-    switch (size) {
-      case 's':
-        setIconSize((prv) => prv + ' text-lg w-8 h-8');
-        break;
-
-      case 'm':
-        setIconSize((prv) => prv + ' text-2xl w-10 h-10');
-        break;
-
-      case 'l':
-        setIconSize((prv) => prv + ' text-5xl w-20 h-20');
-        break;
-
-      default:
-        break;
-    }
-  }, [size]);
-
-  return <div className={iconSize}>{!userName ? <UserOutlined /> : <span>{userName.at(0).toUpperCase()}</span>}</div>;
+const sizeClassNamesDict = {
+  s: 'text-lg w-8 h-8',
+  m: 'text-2xl w-10 h-10',
+  l: 'text-5xl w-20 h-20',
 };
 
-UserIcon.propTypes = { userName: PropTypes.string, size: PropTypes.string };
+const UserIcon = ({ userName, size = 's' }) => {
+  const sizeClassNames = sizeClassNamesDict[size] ?? '';
+
+  return (
+    <div
+      className={`absolute left-0 bottom-0 bg-blue-500 rounded-full flex justify-center items-center ${sizeClassNames}`}
+    >
+      {!userName ? <UserOutlined /> : <span>{userName.at(0).toUpperCase()}</span>}
+    </div>
+  );
+};
+
+UserIcon.propTypes = {
+  userName: PropTypes.string,
+  size: PropTypes.string,
+};
 
 export default UserIcon;
