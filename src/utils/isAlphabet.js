b4e7b03f1f4b5d4429421765a1e100783f2b6545
Will Kim <will@chequer.io>
2022-12-04 04:33:30 +0900

    fix

--- src/utils/isAlphabet.js
+++ src/utils/isAlphabet.js
@@ -14,7 +14,7 @@ export function isAlphabet(char) {
 
   const ascii = char.charCodeAt(0);
   const isUpperCase = ascii >= ASCII.A && ascii <= ASCII.Z;
-  const isLowerCase = ascii >= ASCII.A && ascii <= ASCII.Z;
+  const isLowerCase = ascii >= ASCII.a && ascii <= ASCII.z;
 
   return isLowerCase || isUpperCase;
 }
