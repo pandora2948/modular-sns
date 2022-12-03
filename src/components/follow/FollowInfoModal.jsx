72455c938ad89b36047ed5d3dfc97a4af8a96db8
최은기 <galaxyhi4276@gmail.com>
2022-12-03 20:52:01 +0900

    wip: 팔로우 모달 작업 - 2

--- src/components/follow/FollowInfoModal.jsx
+++ src/components/follow/FollowInfoModal.jsx
@@ -1,14 +1,17 @@
 import PropTypes from 'prop-types';
 import { Modal } from 'antd';
+import shortid from 'shortid';
+import FollowUserItem from './FollowUserItem';
 import useFetchFollowList from './hooks/useFetchFollowList';
 
 const FollowInfoModal = ({ isShow, handleOpen, handleClose }) => {
   const [followUsernameList] = useFetchFollowList();
-  console.log(followUsernameList);
 
   return (
     <Modal title="팔로우 현황" open={isShow} onOk={handleOpen} onCancel={handleClose}>
-      {followUsernameList.map()}
+      {followUsernameList.map((username) => (
+        <FollowUserItem username={username} key={shortid()} />
+      ))}
     </Modal>
   );
 };
--- src/components/follow/FollowUserItem.jsx
+++ src/components/follow/FollowUserItem.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
+import { UserService } from '../../api/services';
+import UserIcon from '../userPanel/UserIcon';
 
 const FollowUserItem = ({ username }) => {
+  const onClickRemoveFollowInfo = () => UserService.removeFollow({ username });
+
   return (
-    <li>
-      <span>{username}</span>
-      <Button>팔로우 취소</Button>
+    <li className="w-full flex justify-between">
+      <div className="flex items-center">
+        <UserIcon username={username} />
+        <span className="ml-2.5 font-semibold">{username}</span>
+      </div>
+      <Button onClick={onClickRemoveFollowInfo}>팔로우 취소</Button>
     </li>
   );
 };
