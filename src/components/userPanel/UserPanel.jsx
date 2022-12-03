3d48485d236b0df48981c3d32a792d2555566d76
최은기 <galaxyhi4276@gmail.com>
2022-12-03 15:08:50 +0900

    feat: 사용자 프로필 접근 작성

--- src/components/userPanel/UserPanel.jsx
+++ src/components/userPanel/UserPanel.jsx
@@ -1,52 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { message } from 'antd';
-import { useRecoilValue } from 'recoil';
 import shortid from 'shortid';
-import { UserService } from '../../api/services';
-import { loginInfoState } from '../auth/SignInForm';
 import ProfileStat from './ProfileStat';
 import UserConfigDropdown from './UserConfigDropdown';
 import UserIcon from './UserIcon';
 
-const userStatInitialData = {
-  allFollowerCount: 0,
-  allFollowingCount: 0,
-  allGivenLikeCount: 0,
-  allPostCount: 0,
-  userInfo: {
-    userId: 0,
-    email: '',
-    username: '',
-  },
-};
-
 const UserPanel = ({ userStatus }) => {
-  const loginInfo = useRecoilValue(loginInfoState);
-  const [userStat, setUserStat] = useState(userStatus);
-
-  useEffect(() => {
-    UserService.getLoginedUser()
-      .then((userStat) => setUserStat(userStat))
-      .catch((e) => message.error(e));
-  }, []);
+  const [userStat] = useState(userStatus);
 
   const contents = [
     { title: '팔로잉', count: userStat.allFollowingCount },
     { title: '팔로워', count: userStat.allFollowerCount },
   ];
 
-  const userName = loginInfo.userName;
-
   return (
     <>
       <section className="bg-gray-200 my-8 px-8 pt-8 py-3">
         <div className="flex flex-col items-start">
           <div className="flex flex-row justify-end align-middle w-full relative">
-            <UserIcon userName={userName} size="l" />
+            <UserIcon userName={userStat.userInfo.username} size="l" />
             <UserConfigDropdown />
           </div>
-          <span className="font-bold text-lg mt-4">{userName}</span>
+          <span className="font-bold text-lg mt-4">{userStat.userInfo.username}</span>
         </div>
         <div className="flex mt-2">
           {contents.map((v) => {
