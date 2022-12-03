16c2eafb63af8575a9427ec0b529d572e58058f9
최은기 <galaxyhi4276@gmail.com>
2022-12-03 20:34:23 +0900

    refactor: userPanel delete array dependencies

--- src/components/userPanel/UserPanel.jsx
+++ src/components/userPanel/UserPanel.jsx
@@ -1,40 +1,39 @@
 import PropTypes from 'prop-types';
-import shortid from 'shortid';
+import { useModal } from '../../hooks/useModal';
+import FollowInfoModal from '../follow/FollowInfoModal';
 import ProfileStat from './ProfileStat';
 import UserConfigDropdown from './UserConfigDropdown';
 import UserIcon from './UserIcon';
 
 const UserPanel = ({ userStatus }) => {
   const { allFollowingCount, allFollowerCount, userInfo, allGivenLikeCount, allPostCount } = userStatus;
-
-  const contents = [
-    { title: '팔로잉', count: allFollowingCount },
-    { title: '팔로워', count: allFollowerCount },
-  ];
+  const { isModalOpen: isFollowModalOpen, openModal: openFollowModal, closeModal: closeFollowModal } = useModal();
+  const { isModalOpen: isFollowingModalOpen } = useModal();
 
   return (
-    <section className="bg-gray-200 my-8 px-8 pt-8 py-3">
-      <div className="flex flex-col items-start">
-        <div className="flex flex-row justify-end align-middle w-full relative">
-          <UserIcon
-            username={userInfo.username}
-            size="l"
-            style={{
-              position: 'absolute',
-              left: 0,
-              bottom: 0,
-            }}
-          />
-          <UserConfigDropdown />
+    <>
+      <section className="bg-gray-200 my-8 px-8 pt-8 py-3">
+        <div className="flex flex-col items-start">
+          <div className="flex flex-row justify-end align-middle w-full relative">
+            <UserIcon
+              username={userInfo.username}
+              size="l"
+              style={{
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+              }}
+            />
+            <UserConfigDropdown />
+          </div>
+          <div className="flex mt-2">
+            <ProfileStat title="팔로잉" count={allFollowingCount} onClick={openFollowModal} />
+            <ProfileStat title="팔로워" count={allFollowerCount} onClick={openFollowModal} />
+          </div>
         </div>
-        <span className="font-bold text-lg mt-4">{userInfo.username}</span>
-      </div>
-      <div className="flex mt-2">
-        {contents.map((v) => {
-          return <ProfileStat key={shortid.generate()} title={v.title} count={v.count} />;
-        })}
-      </div>
-    </section>
+      </section>
+      <FollowInfoModal handleOpen={openFollowModal} handleClose={closeFollowModal} isShow={isFollowModalOpen} />
+    </>
   );
 };
 
