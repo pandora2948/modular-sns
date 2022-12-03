0233c06f70262292b75006160553f9f7cfc7eec3
Will Kim <will@chequer.io>
2022-12-03 18:24:04 +0900

    feat: post 작성 버튼 z-index 조정

--- src/components/post/postForm/PostForm.jsx
+++ src/components/post/postForm/PostForm.jsx
@@ -42,11 +42,14 @@ const PostForm = () => {
   return (
     <>
       <Button
-        className="fixed right-6 bottom-6 w-14 h-14"
+        className="fixed right-6 bottom-6 w-14 h-14 z-[9999]"
         type="primary"
         shape="circle"
         icon={<FormOutlined className="text-2xl" />}
         onClick={openModal}
+        style={{
+          display: isModalOpen ? 'none' : 'initial',
+        }}
       />
 
       <Modal
