e3ba11784e68d61eaf1b177fe5af5904df6d9de9
최은기 <galaxyhi4276@gmail.com>
2022-12-03 20:35:36 +0900

    feat: ProfileStat.jsx onClick 프로퍼티 추가

--- src/components/userPanel/ProfileStat.jsx
+++ src/components/userPanel/ProfileStat.jsx
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 import { Button } from 'antd';
 
-const ProfileStat = ({ title, count }) => {
+const ProfileStat = ({ title, count, onClick }) => {
   return (
-    <Button type="text" size="small" className="flex align-middle leading-normal p-0 mr-1">
+    <Button type="text" size="small" className="flex align-middle leading-normal p-0 mr-1" onClick={onClick}>
       <span className="text-base text-center font-bold mr-1 align-middle">{count}</span>
       <span className="text-base align-middle">{title}</span>
     </Button>
@@ -11,8 +11,9 @@ const ProfileStat = ({ title, count }) => {
 };
 
 ProfileStat.propTypes = {
-  title: PropTypes.string,
-  count: PropTypes.number,
+  title: PropTypes.string.isRequired,
+  count: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default ProfileStat;
